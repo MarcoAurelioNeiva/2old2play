e9e23a9b137a4a7773e64ee68ec319d3ee893c64
MarcoAurelioNeiva <marco_neiva@hotmail.com>
2017-11-17 15:57:43 -0200

    inserindo o modal

--- index.php
+++ index.php
@@ -144,6 +144,81 @@
   @media(max-width: 100%){
     ul{ padding: 0 0 0 0; }
   }
+
+/*modal*/
+
+#myimg{
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+#myimg:hover {opacity: 0.7;}
+
+.modal{
+  display:none;
+  position:fixed;
+  z-index: 1;
+  padding-top: 100px;
+  left:0;
+  top:0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.9);
+}
+.modal-content{
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+}
+#caption{
+  margin:auto;
+  display:block;
+  width: 80%;
+  max-width: 700px;
+  text-align:center;
+  color: #ccc;
+  padding: 10px 0;
+  height: : 150px;
+}
+.modal-content,#caption{
+  -webkit-animation-name: zoom;
+  -webkit-animation-duration: 0.6s
+  animation-name:zoom;
+  animation-duration:0.6s;
+}
+@-webkit-keyframes zoom{
+  from {-webkit-transform:scale(0)}
+  to {-webkit-transform:scale(1);}
+}
+@keyframes zomm {
+  from{transform: scale(0)}
+to {transform:scale(1)}
+}
+.close{
+  position:absolute;
+  top:15px;
+  right:35px;
+  color: #f1f1f1;
+  font-size: 40px;
+  font-weight: bold;
+  transition: 0.3s;
+}
+.close:hover,
+.close:focus{
+  color:#bbb;
+  text-decoration: none;
+  cursor:pointer;
+@media only screen and (max-width: 700px){
+  .modal-content{
+    width: 100%;
+  }
+}
+
+}
+
 </style>
 </head>
 <body style="background-color:#aaaaaa; margin:0px">
@@ -228,6 +303,7 @@
   <span class="dot" onclick="currentSlide(3)"></span> 
 </div>
 
+
 <script>
   /*var slideIndex = 0;
   showSlides(slideIndex);
@@ -304,5 +380,30 @@ function avance(array){
   </div>
   <!-- ------------------------------------------------------------ -->
 
+<!--modal -->
+
+<div id="mymodal" class="modal">
+  <span class="close">&times;</span>
+  <img src="g3.jpg" width="40%" id="img01">
+  <div id="capition"></div>
+</div>
+
+<script>
+  var modal = document.getElementById('mymodal');
+  var img = document.getElementById('myimg');
+  var modalimg = document.getElementByid("img01");
+  var captiontext= document.getElementById("caption");
+
+  img.inclick = function(){
+    modal.style.dysplay ="block";
+    modalimg.src = this.src;
+    captiontext,innerHTML = this.alt;
+  }
+var span = document.getElementsByClassName("close") [0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+</script>
 </body>
 </html>
