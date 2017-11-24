818ac352920b3bc8be725635a58ad7412e9eb227
MarcoAurelioNeiva <marco_neiva@hotmail.com>
2017-11-24 11:40:03 -0200

    background,add all img, ajuste mini

--- imagens.php
+++ imagens.php
@@ -3,15 +3,19 @@
 <head>
 	<title>Imagens</title>
 	<style>
+	body{
+		background-color: rgb(180, 182, 216);
+	}
 	ul{
 		width: 800px;
 		list-style: none;
 		display: flex;
-		margin: 100px auto;
+		margin: 50px 10px;
 	}
-	.mim{
+	.mini{
 		width: 200px;
-		padding: 10px;
+		height: 136px;
+		padding: 0px 0px 0px 10px;
 	}
 	.lbox{
 		visibility: hidden;
@@ -72,17 +76,39 @@
 </head>
 <body>
 	<ul>
+		<li><a href="#img0"><img src="g0.png" class="mini"></a></li>
 		<li><a href="#img1"><img src="g1.jpg" class="mini"></a></li>
 		<li><a href="#img2"><img src="g2.jpg" class="mini"></a></li>
 		<li><a href="#img3"><img src="g3.jpg" class="mini"></a></li>
 		<li><a href="#img4"><img src="g4.jpg" class="mini"></a></li>
+	</ul>
+	<ul>
 		<li><a href="#img5"><img src="g5.jpg" class="mini"></a></li>
 		<li><a href="#img6"><img src="g6.jpg" class="mini"></a></li>
-	</ul>		
+		<li><a href="#img7"><img src="g7.jpg" class="mini"></a></li>
+		<li><a href="#img8"><img src="g8.jpg" class="mini"></a></li>
+		<li><a href="#img9"><img src="g9.jpg" class="mini"></a></li>
+	</ul>
+	<ul>
+		<li><a href="#img10"><img src="g10.jpg" class="mini"></a></li>
+		<li><a href="#img11"><img src="g11.jpg" class="mini"></a></li>
+		<li><a href="#img12"><img src="g12.jpg" class="mini"></a></li>
+		<li><a href="#img13"><img src="g13.jpg" class="mini"></a></li>
+		<li><a href="#img14"><img src="g14.jpg" class="mini"></a></li>
+	</ul>
+		
+	<div class="lbox" id="img0">
+		<div class="box_img">
+			<a href="#img14" class="btn" id="prev">&#171;</a>
+			<a href="" class="btn" id="close">&times;</a>
+			<img src="g0.png" class="imagem">
+			<a href="#img1" class="btn" id="next">&#187;</a>
+		</div>
+	</div>
 
 	<div class="lbox" id="img1">
 		<div class="box_img">
-			<a href="" class="btn" id="prev">&#171;</a>
+			<a href="#img0" class="btn" id="prev">&#171;</a>
 			<a href="" class="btn" id="close">&times;</a>
 			<img src="g1.jpg" class="imagem">
 			<a href="#img2" class="btn" id="next">&#187;</a>
@@ -133,5 +159,79 @@
 			<a href="#img7" class="btn" id="next">&#187;</a>
 		</div>
 	</div> 
+
+	<div class="lbox" id="img7">
+		<div class="box_img">
+			<a href="#img6" class="btn" id="prev">&#171;</a>
+			<a href="" class="btn" id="close">&times;</a>
+			<img src="g7.jpg" class="imagem">
+			<a href="#img8" class="btn" id="next">&#187;</a>
+		</div>
+	</div>
+
+	<div class="lbox" id="img8">
+		<div class="box_img">
+			<a href="#img7" class="btn" id="prev">&#171;</a>
+			<a href="" class="btn" id=close>&times;</a>
+			<img src="g8.jpg" class="imagem">
+			<a href="#img9" class="btn" id="next">&#187;</a>
+		</div>
+	</div>
+
+	<div class="lbox" id="img9">
+		<div class=box_img>
+			<a href="#img8" class="btn" id="prev">&#171;</a>
+			<a href="" class="btn" id="close">&times;</a>
+			<img src="g9.jpg" class="imagem">
+			<a href="#img10" class=btn id="next">&#187;</a>
+		</div>
+	</div>
+
+	<div class="lbox" id="img10">
+		<div class="box_img">
+			<a href="#img9" class="btn" id="prev">&#171;</a>
+			<a href="" class="btn" id="close">&times;</a>
+			<img src="g10.jpg" class="imagem">
+			<a href="#img11" class="btn" id="next">&#187;</a>
+		</div>		
+	</div>
+
+	<div class="lbox" id="img11">
+		<div class="box_img">
+			<a href="#img10" class="btn" id="prev">&#171;</a>
+			<a href="" class="btn" id="close">&times;</a>
+			<img src="g11.jpg" class="imagem">
+			<a href="#img12" class="btn" id="next">&#187;</a>
+		</div>
+	</div>
+
+	<div class="lbox" id ="img12">
+		<div class="box_img">
+			<a href="#img11" class="btn" id="prev">&#171;</a>
+			<a href="" class="btn" id="close">&times;</a>
+			<img src="g12.jpg" class="imagem">
+			<a href="#img13" class="btn" id="next">&#187;</a>
+		</div>
+	</div>
+
+	<div class="lbox" id="img13">
+		<div class="box_img">
+			<a href="#img12" class="btn" id="prev">&#171;</a>
+			<a href="" class="btn" id="close">&times;</a>
+			<img src="g13.jpg" class="imagem">
+			<a href="#img14" class="btn" id="next">&#187;</a>
+		</div> 
+	</div>
+
+	<div class="lbox" id="img14">
+		<div class=box_img>
+			<a href="#img13" class="btn" id="prev">&#171;</a>
+			<a href="" class="btn" id="close">&times;</a>
+			<img src="g14.jpg" class="imagem">
+			<a href="#img0" class="btn" id="next">&#187;</a>
+		</div>
+	</div>
+
+	
 </body>
 </html>
